ad25a4a978a1374a0e101206fdff8e99f82b6657
haze.liu <liu19891014@163.com>
2015-11-26 22:53:00 +0800

    1.删除无用文件 2.添加pajax 3.billList 完成20% 前台

---
+++ www/lib/jplusin/jquery.pajax.custom.js
@@ -0,0 +1,120 @@
+/**
+ * @todo pajax  ajax返回上一步
+ * @namespace pajax
+ * @author haze.liu
+ * @since 2015年8月20日 下午2:34:22
+ */
+(function($) {
+	/**
+	 * 方法说明<BR>
+	 * load :加载指定页面 当第二个参数为true是代码可以回退  $.pajax('load','demo1',true)  or $.pajax('load','demo1');
+	 * back :返回上一个页面
+	 */
+
+	/**
+	 * dom说明<BR>
+	 * 1.带pa-href 属性的映射为单页跳转
+	 * <a pa-href="demo2"  class="btn btn-primary">点我跳demo2</a>
+	 * 2.带pa-back ，可以返回到上一页的，如果没有则反之
+	 * <a pa-href="demo1" pa-back class="btn btn-primary">点我跳demo1(可以返回)</a>
+	 */
+	/**
+	 * 参数说明<BR>
+	 * mainContainer : 主容器(默认为mainContainer)
+	 * suffix :后缀名(默认为html)
+	 * defaultLoad:默认加载(默认为index)
+	 * 
+	 */
+
+
+
+	 var defaults = {
+		mainContainer:"mainContainer",
+		suffix:"html",
+		defaultLoad:"index",
+	};
+	var methods = null;
+	var _methods = null;
+
+
+	methods = {
+		init : function(options) {
+			defaults = $.extend(defaults, options);
+			_methods._initEvent();
+			_methods._load(defaults.defaultLoad);
+		},
+		load :function(url,isBack){
+			_methods._load(url,isBack);
+		},
+		back :function(){
+			_methods._back();
+		}
+
+	};
+	_methods ={
+		_initEvent:function(){
+			// $('*[pa-href]').on('click', function(event) {
+			$('body').delegate('*[pa-href]', 'click', function(event) {
+				var $this=$(this)
+				var href = $this.attr("pa-href");
+				if($this.attr("pa-back")!=undefined && $this.attr("pa-back") ==''){
+					_methods._load(href,true);
+				}else{
+					_methods._load(href);
+				}
+				
+			});
+			if (history.pushState) {
+				window.addEventListener("popstate", function(e){
+					_methods._changeHistory();
+				});
+			}
+		},
+		_changeHistory:function(){
+			var href=_methods._getParam('paHref');
+			if(href){
+				_methods._load(href);
+				//custom
+				$("header .leftBtn .fa-bars").removeClass('fa-bars').addClass('fa-arrow-left');
+			}else{
+				_methods._load(defaults.defaultLoad);
+			}
+		},
+		_getParam:function(name){
+			var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+			var r = window.location.search.substr(1).match(reg);
+			if (r != null)
+				return unescape(r[2]);
+			return null;
+		},
+		_load:function(url,isBack){
+			$("#"+defaults.mainContainer).load(url+"."+defaults.suffix);
+			if(isBack){
+				history.pushState({isBack:isBack}, document.title, location.href.split("?")[0] + "?paHref=" + url);
+				//custom
+				$("header .leftBtn .fa-bars").removeClass('fa-bars').addClass('fa-arrow-left');
+			}else{
+				//custom
+				$("header .leftBtn .fa-arrow-left").removeClass('fa-arrow-left').addClass('fa-bars');
+			}
+		},
+		_back:function(){
+			history.back();
+		}
+			
+	}
+	$.pajax = function() {
+		var method = arguments[0];
+		if (methods[method]) {
+			method = methods[method];
+			arguments = Array.prototype.slice.call(arguments, 1);
+		} else if (typeof (method) == 'object' || !method) {
+			method = methods.init;
+		} else {
+			$.error('Method ' + method + ' does not exist on jQuery.pajax');
+			return this;
+		}
+		return method.apply(this, arguments);
+	};
+	// $.pajax.defaults = {};
+})(jQuery);
